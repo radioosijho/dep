04029b65fdffa283f0fb09d040d786c3860604ed
radiossijho <92832108+radioosijho@users.noreply.github.com>
2023-08-05 18:47:04 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,73 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_modelo-360_3_2023-08-05-17-41-30",
+      "name": "Panorama_Modelo 360_3_2023-08-05-17-41-30",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-panorama_modelo-360_3_2023-08-05-18-30-17",
+      "name": "Panorama_Modelo 360_3_2023-08-05-18-30-17",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
